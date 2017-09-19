e714eb6ce05a580f85e06eb27dc16c2df079d67e
Mathieu Clement <mclement2@dons.usfca.edu>
2017-09-18 17:29:15 -0700

    Write a test to check that chunking works.

--- test/main/java/edu/usfca/cs/dfs/structures/ChunkTest.java
+++ test/main/java/edu/usfca/cs/dfs/structures/ChunkTest.java
@@ -2,11 +2,13 @@ package edu.usfca.cs.dfs.structures;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 
-import static edu.usfca.cs.dfs.structures.Chunk.calculateLastChunkSize;
-import static edu.usfca.cs.dfs.structures.Chunk.calculateNumberOfChunks;
-import static edu.usfca.cs.dfs.structures.Chunk.createChunksFromFile;
+import static edu.usfca.cs.dfs.structures.Chunk.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ChunkTest {
@@ -32,6 +34,50 @@ class ChunkTest {
 
     @Test
     void testCreateChunksFromFile() throws IOException {
-        createChunksFromFile("/tmp/input_file", 8 /*  bytes */, "/tmp/chunks");
+        String inputFilename = "/tmp/input_file";
+        String inputString = "Hello, my name is R2-D2.\n";
+        writeStringToFile(inputFilename, inputString);
+
+        String outputDirectory = "/tmp/chunks";
+        File outputDirectoryFile = new File(outputDirectory);
+        if (outputDirectoryFile.exists()) {
+            // Failsafe in case somebody doesn't actually read this test and changes outputDirectory
+            // to a directory with valuable files in it...
+            throw new IllegalStateException("Directory " + outputDirectory + " already exists.");
+        }
+        outputDirectoryFile.mkdir();
+        try {
+            createChunksFromFile(inputFilename, 8 /*  bytes */, outputDirectory);
+
+            byte[] actualBytes = new byte[inputString.getBytes().length];
+            int counter = 0;
+            for (String s : outputDirectoryFile.list()) {
+                byte[] sBytes = readBytesFromFile(new File(outputDirectory, s));
+                for (int i = 0; i < sBytes.length; i++) {
+                    actualBytes[counter++] = sBytes[i];
+                }
+            }
+            assertArrayEquals(inputString.getBytes(), actualBytes);
+        } finally {
+            deleteAllFilesFromDirectory(outputDirectoryFile);
+            outputDirectoryFile.delete();
+        }
+    }
+
+    private byte[] readBytesFromFile(File file) throws IOException {
+        return Files.readAllBytes(file.toPath());
+    }
+
+    private void writeStringToFile(String filename, String str) throws IOException {
+        FileWriter inputFileWriter = new FileWriter(filename);
+        inputFileWriter.write(str);
+        inputFileWriter.close();
+    }
+
+    private void deleteAllFilesFromDirectory(File outputDirectoryFile) {
+        String[] entries = outputDirectoryFile.list();
+        for (String s : entries) {
+            new File(outputDirectoryFile.getPath(), s).delete();
+        }
     }
 }

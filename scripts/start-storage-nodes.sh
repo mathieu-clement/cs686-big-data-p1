abb32ad509e9a772133c9890a294071b56983224
Mathieu Clement <mclement2@dons.usfca.edu>
2017-10-10 09:11:25 -0700

    Start process in the background and check argument

--- scripts/start-storage-nodes.sh
+++ scripts/start-storage-nodes.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+then
+    echo "missing argument: controller-host"
+    exit 1
+fi
+
 for i in 14 15 16 17 18 20 21 22 23 24
 do
 echo "Starting storage node on bass${i}..."
 ssh bass${i} controller_host=$1 storage_node_host=bass${i} 'bash -s' << 'ENDSSH'
-./storage-node.sh $controller_host > logs/storage-node-$storage_node_host
+nohup ./storage-node.sh $controller_host > logs/storage-node-$storage_node_host &
 ENDSSH
 done

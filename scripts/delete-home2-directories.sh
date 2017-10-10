f4307546c7fe5adfbab16f0cfc5d84852e5efd25
Mathieu Clement <mclement2@dons.usfca.edu>
2017-10-10 09:04:58 -0700

    Show name of node

--- scripts/delete-home2-directories.sh
+++ scripts/delete-home2-directories.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 for i in `seq -w 1 24`
 do
+echo bass${i}
 ssh bass${i} 'bash -s' << 'ENDSSH'
 rm -rf /home2/mclement2/dfs*
 ENDSSH
--- scripts/kill-storage-nodes.sh
+++ scripts/kill-storage-nodes.sh
@@ -2,5 +2,6 @@
 
 for i in `seq -w 1 24`
 do
+echo bass${i}
 ssh bass${i} './kill-storage-node.sh'
 done
